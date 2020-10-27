16ade849681556e1a2cc654c2656efc27186f5a9
xiexinwei <41041750+GarbageCodeMaker@users.noreply.github.com>
2020-10-27 16:30:48 +0800

    测试git

--- src/components/home/Universe.vue
+++ src/components/home/Universe.vue
@@ -54,7 +54,7 @@ export default {
       light.position.set(600, 600, 600)
       this.scene.add(light)
     },
-    initObject () { // 几何体
+    initObject () { // 初始化几何体
       /* 太阳 */
       const sun = new Three.SphereGeometry(100, 100, 100)
       new Three.TextureLoader()
